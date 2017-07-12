881e9b2f1c913e27d986e16ee4b29f54b8fc7ec6
jtrauer <james.trauer@monash.edu>
2017-07-12 14:54:29 +1000

    Move running code to new file called interface

---
+++ autumn_model/interface.py
@@ -0,0 +1,29 @@
+
+from model_runner import ModelRunner
+from outputs import Project
+
+
+time_early_treatment = 1. / 52.
+time_late_treatment = .5 - time_early_treatment
+fixed_parameters = {
+    'demo_rate_birth': 20. / 1e3,
+    'demo_rate_death': 1. / 65,
+    'tb_n_contact': 60.,
+    'tb_rate_earlyprogress': .1 / .5,
+    'tb_rate_lateprogress': .1 / 20.,
+    'tb_rate_stabilise': .9 / .5,
+    'tb_rate_recover': .6 / 3.,
+    'tb_rate_death': .4 / 3.,
+    'program_rate_completion_infect': .98 / time_early_treatment,
+    'program_rate_default_infect': .01 / time_early_treatment,
+    'program_rate_death_infect': .01 / time_early_treatment,
+    'program_rate_completion_noninfect': .9 / time_late_treatment,
+    'program_rate_default_noninfect': .05 / time_late_treatment,
+    'program_rate_death_noninfect': .05 / time_late_treatment,
+    'int_vaccine_efficacy': .5
+}
+model_runner \
+    = ModelRunner(country='India', fixed_parameters=fixed_parameters, mode='uncertainty', scenarios_to_run=[0, 1, 2])
+model_runner.master_runner()
+project = Project(model_runner)
+project.master_outputs_runner()
--- autumn_model/model_runner.py
+++ autumn_model/model_runner.py
@@ -186,7 +186,7 @@ class ModelRunner:
         prev_log_likelihood = -1e10
 
         # instantiate uncertainty model object
-        self.model_dict['uncertainty'] = tb_model.SimpleTbModel(fixed_parameters, self.inputs, 0)
+        self.model_dict['uncertainty'] = tb_model.SimpleTbModel(self.fixed_parameters, self.inputs, 0)
 
         # until a sufficient number of parameters are accepted
         run = 0
@@ -397,30 +397,5 @@ class ModelRunner:
         raise ValueError('Time not found')
 
 
-if __name__ == '__main__':
-    time_early_treatment = 1. / 52.
-    time_late_treatment = .5 - time_early_treatment
-    fixed_parameters = {
-        'demo_rate_birth': 20. / 1e3,
-        'demo_rate_death': 1. / 65,
-        'tb_n_contact': 60.,
-        'tb_rate_earlyprogress': .1 / .5,
-        'tb_rate_lateprogress': .1 / 20.,
-        'tb_rate_stabilise': .9 / .5,
-        'tb_rate_recover': .6 / 3.,
-        'tb_rate_death': .4 / 3.,
-        'program_rate_completion_infect': .98 / time_early_treatment,
-        'program_rate_default_infect': .01 / time_early_treatment,
-        'program_rate_death_infect': .01 / time_early_treatment,
-        'program_rate_completion_noninfect': .9 / time_late_treatment,
-        'program_rate_default_noninfect': .05 / time_late_treatment,
-        'program_rate_death_noninfect': .05 / time_late_treatment,
-        'int_vaccine_efficacy': .5
-    }
-    model_runner \
-        = ModelRunner(country='India', fixed_parameters=fixed_parameters, mode='uncertainty', scenarios_to_run=[0, 1, 2])
-    model_runner.master_runner()
-    project = Project(model_runner)
-    project.master_outputs_runner()
 
 
